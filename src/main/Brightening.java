9633a05815d2383c38d5510bc48463e7d4a3387d
Ahmad Jailani <ahmadjailani604@gmail.com>
2021-11-16 11:00:26 +0700

    Update Brightening class

--- src/main/Brightening.java
+++ src/main/Brightening.java
@@ -1,17 +1,17 @@
 package main;
 
-import java.awt.Color;
+import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import javax.imageio.ImageIO;
-import java.io.IOException;
 
 public class Brightening {
-     static BufferedImage image = null;
-    
-     public static void main(String[]args){
+    static BufferedImage image = null;
+
+    public static void main(String[] args) {
         brightening();
     }
+
     private static void readImage(String fileName) {
         try {
             File inputFile = new File("C:\\Users\\ahmad\\IdeaProjects\\TugasPengCit\\src\\assets\\" + fileName + ".jpg");
@@ -25,40 +25,37 @@ public class Brightening {
 
     private static void writeImage(BufferedImage image, String fileName) {
         try {
-            File outputFile = new File(""C:\\Users\\ahmad\\IdeaProjects\\TugasPengCit\\src\\output\\" + fileName + ".jpg");
+            File outputFile = new File("C:\\Users\\ahmad\\IdeaProjects\\TugasPengCit\\src\\output\\" + fileName + ".jpg");
             ImageIO.write(image, "jpg", outputFile);
             System.out.println("Writing complete");
         } catch (Exception e) {
             System.out.println("Error: " + e);
         }
     }
-   
-        
 
-       private static void brightening(){
-
+    private static void brightening() {
         try {
-            readImage("2018");
+            readImage("66075");
 
             int width = image.getWidth();
             int height = image.getHeight();
-            
+
             for (int i = 0; i < height; i++) {
                 for (int j = 0; j < width; j++) {
                     Color c = new Color(image.getRGB(j, i));
-                    int col = (int) ((c.getRed() + c.getGreen() + c.getBlue())/3 );
+                    int col = (c.getRed() + c.getGreen() + c.getBlue()) / 3;
                     int hasil = col + 100;
                     if (hasil > 255) {
                         hasil = 255;
                     } else if (hasil < 0) {
                         hasil = 0;
-                    } 
+                    }
                     Color newColor = new Color(hasil, hasil, hasil);
                     image.setRGB(j, i, newColor.getRGB());
                 }
             }
-            
-            writeImage(image, "aritmatika_gbr1");
+
+            writeImage(image, "brightening_gbr1");
         } catch (Exception e) {
             System.out.println("Error: " + e);
         }
