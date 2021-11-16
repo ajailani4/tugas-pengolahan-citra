f0228a1440eded6713b80ab9ad34a2bdf9608864
Ahmad Jailani <ahmadjailani604@gmail.com>
2021-11-16 11:06:07 +0700

    Update Gaussian class

--- src/main/Gaussian.java
+++ src/main/Gaussian.java
@@ -1,27 +1,25 @@
 package main;
 
 import javax.imageio.ImageIO;
-import javax.imageio.ImageReader;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.IOException;
 
 public class Gaussian {
     static BufferedImage image = null;
 
     public static void main(String[] args) {
-        readImage();
-        image = apply(image, 7, 0.84089642);
+        readImage("46076");
+        image = apply(image, 7, 1.0);
         writeImage(image, "gaussian_gbr1");
     }
 
-    private static void readImage() {
+    private static void readImage(String fileName) {
         try {
-            File inputFile = new File("C:\\Users\\ahmad\\IdeaProjects\\TugasPengCit\\src\\assets\\Prince_Zuko.jpg");
+            File inputFile = new File("C:\\Users\\ahmad\\IdeaProjects\\TugasPengCit\\src\\assets\\" + fileName + ".jpg");
             image = ImageIO.read(inputFile);
 
             System.out.println("Reading complete");
-        } catch(Exception e) {
+        } catch (Exception e) {
             System.out.println("Error: " + e);
         }
     }
@@ -38,7 +36,7 @@ public class Gaussian {
 
     public static double[][] kernel(int _size, double sigma) {
         int size = _size;
-        double gaussian[][] = new double[size][size];
+        double[][] gaussian = new double[size][size];
         for (int j = 0; j < size; j++) {
             for (int i = 0; i < size; i++) {
                 int xValue = i - (size / 2);
@@ -65,9 +63,7 @@ public class Gaussian {
 
         BufferedImage imageOutput = new BufferedImage(_image.getWidth(), _image.getHeight(), BufferedImage.TYPE_3BYTE_BGR);   // Set initial BufferedImage
 
-        int c = 0;
-
-        double gaussian[][] = Gaussian.kernel(size, sigma);
+        double[][] gaussian = Gaussian.kernel(size, sigma);
         double agv = Gaussian.kernel_sum(gaussian);
         int wight = _image.getWidth();
         int heigth = _image.getHeight();
@@ -81,19 +77,27 @@ public class Gaussian {
                 for (int k = -kernelXY; k <= kernelXY; k++) {
                     for (int l = -kernelXY; l <= kernelXY; l++) {
                         if (k * k + l * l < gaussian.length * gaussian.length) {
-                            int p = RGB.getRGBW(_image, i + k, j + l);
-                            // calculate a RGB by chip bit
+                            int p = getRGBW(_image, i + k, j + l);
                             r += ((p >> 16) & 0xFF) * gaussian[k + kernelXY][l + kernelXY];
                             g += ((p >> 8) & 0xFF) * gaussian[k + kernelXY][l + kernelXY];
                             b += (p & 0xFF) * gaussian[k + kernelXY][l + kernelXY];
                         }
-                    } //end l
-                }//end k
+                    }
+                }
                 int rgb = ((int) (r / agv) << 16) | ((int) (g / agv) << 8) | ((int) (b / agv));
-                //set RGB revert to image
+
                 imageOutput.setRGB(i, j, rgb);
-            }// end i
-        } //end j;
+            }
+        }
+
         return imageOutput;
+    }
+
+    public static int getRGBW(BufferedImage image, int i, int j) {
+        int width = image.getWidth();
+        int height = image.getHeight();
+        i = Math.max(0, Math.min(width - 1, i));
+        j = Math.max(0, Math.min(height - 1, j));
+        return image.getRGB(i, j);
     }
 }
